a9c6a9d5b4cd1828387f1f095b8d4308b4c0e95f
horejsiantonin <6O651398b>
2020-01-13 19:59:38 +0100

    WpfDesktop + API

---
+++ WPF_Desktop_Robitusin/WPF_Desktop_Robitusin/MainWindow.xaml.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using MySql.Data.MySqlClient;
+
+namespace Wpf_Desktop_Robitusin
+{
+    /// <summary>
+    /// Interakční logika pro MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        MySqlConnection con = new MySqlConnection(@"Data Source=localhost;port=3306;database=robitusin;uid=root;password=;CharSet=utf8;Persist Security Info=True");
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void bt_Submit_Click(object sender, RoutedEventArgs e)
+        {
+            string Query = "";
+            con.Open();
+            MySqlCommand cmd;
+            MySqlDataReader MyReader2;
+            
+            
+            if(tb_Password.Text == tb_PasswordCorr.Text)
+            {
+                User myUser = new User();
+                
+                myUser.fName = tb_fName.Text;
+                myUser.lName = tb_lName.Text;
+                myUser.Email = tb_Email.Text;
+                myUser.Password = tb_Password.Text;
+                
+                Query = "INSERT INTO robitusin.user (Username,Email,Password) Values ('" + tb_fName.Text + tb_lName.Text + "','" + tb_Email.Text + "','" + tb_Password.Text + "')";
+                cmd = new MySqlCommand(Query, con);
+                MyReader2 = cmd.ExecuteReader();
+                while (MyReader2.Read())
+                {
+                }
+                con.Close();
+            }
+            else
+            {
+                MessageBox.Show("Hesla se Neshodují");
+            }
+            
+        }
+
+        private void bt_Reset_Click(object sender, RoutedEventArgs e)
+        {
+            tb_fName.Text = "";
+            tb_lName.Text = "";
+            tb_Email.Text = "";
+            tb_Password.Text = "";
+            tb_PasswordCorr.Text = "";
+        }
+    }
+}
---
+++ WPF_Desktop_Robitusin/WPF_Desktop_Robitusin/User.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Wpf_Desktop_Robitusin
+{
+    public class User
+    {
+        public string fName { get; set; }
+        public string lName { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+    }
+}
