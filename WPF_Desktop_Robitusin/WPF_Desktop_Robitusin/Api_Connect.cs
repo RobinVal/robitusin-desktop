13051e9e0cb9ba4c2b3c484369c8938bacbcd713
horejsiantonin <6O651398b>
2020-01-14 04:16:00 +0100

    Wpf Log + Reg Design NoApi

---
+++ WPF_Desktop_Robitusin/WPF_Desktop_Robitusin/Api_Connect.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Wpf_Desktop_Robitusin
+{
+    class Api_Connect
+    {
+    }
+}
